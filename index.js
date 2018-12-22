b7790219d80c4a7bce143635fc6c2568fb6ae7ca
eranhess <eran.hess@gmail.com>
2018-12-22 23:38:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-$(".timeline-wrapper .timeline-content-item > span").on("mouseenter mouseleave", function(e){
-  $(".timeline-wrapper .timeline-content-item.active").removeClass("active");
-  $(this).parent().addClass("active");
+var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
 });
